e29b14b2bdcdb54adf260cad01425a7153adfc1f
Francisco Almeida <franciscoalm100@gmail.com>
2020-10-19 16:27:59 +0100

    Plataforma e keys adicionadas
    Plataforma onde o jogador vai repousar, o jogador só pode saltar uma vez.

--- Game/Scenes/Player/Player.gd
+++ Game/Scenes/Player/Player.gd
@@ -17,9 +17,13 @@ func _process(delta):
 	#Como o eixo é invertido queremos a gravidade a ser adicionada.
 	velocity.y += GRAVITY
 	
-	if Input.is_action_just_pressed("ui_accept"):
+	#Podiamos usar a função is_on_wall() e o engine verifica se o player esta a bater num static body.
+	if Input.is_action_just_pressed("Jump_Keys") and is_on_floor():
+		#Jump speed é negativo porque o eixo do jogo é invertido da nossa norma. Para subir temos de decrementar o Y.
 		velocity.y = -JUMP_SPEED
 	
 	#Função especifica de KinematicBody2D  
 	#Moves the body along a vector. If the body collides with another, it will slide along the other body rather than stop immediately
-	move_and_slide(velocity)
+	#Inserimos o Vecto2.UP para dizermos que estamos a aumentar a velocidade para cima na perspectiva do nosso jogo
+	#Assim a função is_on_floor() sabe onde fica o chao, fica no valor negativo da velocity, e começa a funcionar da maneira que queremos para este jogo
+	move_and_slide(velocity,Vector2.UP)
